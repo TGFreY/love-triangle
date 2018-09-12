3ca23f14e5d49308b07e98c83e18e2f7fd9406e7
TGFreY <t.g.frey19@gmail.com>
2018-09-12 20:06:32 +0300

    consider angles converting them into triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let triangles = 0;
+    for (let i = 0; i < preferences.length; i++) {
+        if (i + 1 != preferences[i] && i + 1 == preferences[preferences[preferences[i] - 1] - 1]) {
+            triangles++;
+        }
+    }
+
+    return triangles / 3;
 };
